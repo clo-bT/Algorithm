b6d21906b4ddba18b1b2aa936ae7a0caf9e00b62
승희 <122757589+clo-bT@users.noreply.github.com>
2023-02-09 10:09:50 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 1280 ms, Memory: 121044 KB -BaekjoonHub

---
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.py
@@ -0,0 +1,24 @@
+
+n = int(input()) #5
+def fib(n):
+    if n == 1 or n == 2:
+        return 1
+    else:
+        global cnt_fib 
+        cnt_fib += 1
+        return fib(n-1) + fib(n-2)
+
+def dfib(n):
+    f = [1,1]
+    for i in range(2,n):
+        global cnt_dfib
+        cnt_dfib += 1
+        res = f[i-1] + f[i-2]
+        f.append(res)
+    return f[n-1]
+
+cnt_fib = 1
+cnt_dfib = 0
+fib(n)
+dfib(n)
+print(cnt_fib, cnt_dfib)
