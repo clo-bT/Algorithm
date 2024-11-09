77b4a914fa1dea63356d8f3c5967a48eed434f12
승희 <122757589+clo-bT@users.noreply.github.com>
2024-11-09 23:53:06 +0900

    [level 2] Title: 조이스틱, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.js
@@ -0,0 +1,23 @@
+function solution(name) {
+    let answer = 0;
+    let n = name.length - 1;
+
+    for (let i = 0; i < name.length; i++) {
+        const letter = name.charCodeAt(i) - 'A'.charCodeAt(0);
+        answer += Math.min(letter, 26-letter);
+
+        let nextIndex = i + 1;
+
+        while (nextIndex < name.length && 
+               name.charCodeAt(nextIndex) - 'A'.charCodeAt(0) === 0) {
+          nextIndex += 1;
+        }
+        n = Math.min(
+          n,
+          i * 2 + name.length - nextIndex,
+          i + (name.length - nextIndex) * 2 
+        );
+    }
+    answer += n;
+    return answer;
+}
