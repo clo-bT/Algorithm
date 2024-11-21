df4646cfc235ed0d361f03091dbde520ac95da3f
승희 <122757589+clo-bT@users.noreply.github.com>
2024-11-21 18:27:37 +0900

    [level 0] Title: 로그인 성공?, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
@@ -0,0 +1,11 @@
+function solution(id_pw, db) {
+    for (const [id, pw] of db) {
+        if (id === id_pw[0]) {
+            if (pw === id_pw[1]) {
+                return "login";
+            }
+            return "wrong pw";
+        }
+    }
+    return "fail";
+}
