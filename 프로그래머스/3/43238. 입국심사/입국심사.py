bde9d15d268c7100b364eae2817df93048bde3fc
승희 <122757589+clo-bT@users.noreply.github.com>
2024-11-28 14:53:27 +0900

    [level 3] Title: 입국심사, Time: 581.77 ms, Memory: 14 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -0,0 +1,15 @@
+def solution(n, times):
+    left, right = 1, max(times) * n
+    answer = right
+
+    while left <= right:
+        mid = (left + right) // 2
+        total = sum(mid // time for time in times)
+        
+        if total >= n:
+            answer = mid
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    return answer
