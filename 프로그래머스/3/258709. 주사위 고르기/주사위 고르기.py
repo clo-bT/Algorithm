06fd5d74120e4da33ed71e4a3a954c92284fb8ab
승희 <122757589+clo-bT@users.noreply.github.com>
2024-11-28 19:25:16 +0900

    [level 3] Title: 주사위 고르기, Time: 5567.26 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.py
@@ -0,0 +1,51 @@
+from itertools import combinations, product
+from collections import defaultdict
+
+def solution(dice):
+    n = len(dice)
+    half = n // 2
+    max_rate = 0
+    answer = []
+
+    all_indices = list(combinations(range(n), half))
+
+    for a_indices in all_indices:
+        b_indices = [i for i in range(n) if i not in a_indices]
+        
+        a_dice = [dice[i] for i in a_indices]
+        b_dice = [dice[i] for i in b_indices]
+        
+        a_sum_freq = defaultdict(int)
+        b_sum_freq = defaultdict(int)
+        
+        for a in product(*a_dice):
+            a_sum_freq[sum(a)] += 1
+        
+        for b in product(*b_dice):
+            b_sum_freq[sum(b)] += 1
+        
+        win, lose = 0, 0
+        total_cases = 0
+        
+        for a_sum, a_count in a_sum_freq.items():
+            for b_sum, b_count in b_sum_freq.items():
+                total_cases += a_count * b_count
+                if a_sum > b_sum:
+                    win += a_count * b_count
+                elif a_sum < b_sum:
+                    lose += a_count * b_count
+        
+        draw = total_cases - win - lose
+        win_rate = win / total_cases if total_cases > 0 else 0
+        lose_rate = lose / total_cases if total_cases > 0 else 0
+        
+        if win_rate > lose_rate:
+            if win_rate > max_rate:
+                max_rate = win_rate
+                answer = sorted(a_indices)
+        else:
+            if lose_rate > max_rate:
+                max_rate = lose_rate
+                answer = sorted(b_indices)
+
+    return [i + 1 for i in answer]
