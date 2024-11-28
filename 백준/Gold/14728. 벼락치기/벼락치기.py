eff265c196925aa5681b2ee70ed4782f1209122e
승희 <122757589+clo-bT@users.noreply.github.com>
2024-11-29 03:07:34 +0900

    [Gold V] Title: 벼락치기, Time: 340 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/14728. 벼락치기/벼락치기.py
+++ 백준/Gold/14728. 벼락치기/벼락치기.py
@@ -1,13 +1,10 @@
 N, T = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(N)] 
+arr = [list(map(int, input().split())) for _ in range(N)]
 
-dp = [[0]*(T+1) for _ in range(N+1)]
-for i in range(1,N+1):
-    for j in range(1,T+1):
-        time = arr[i-1][0]
-        score = arr[i-1][1]
-        if j < time:
-            dp[i][j] = dp[i-1][j]
-        else:
-            dp[i][j] = max(score + dp[i-1][j-time], dp[i-1][j])
-print(dp[N][T])
+dp = [0] * (T + 1)
+
+for time, score in arr:
+    for j in range(T, time - 1, -1):
+        dp[j] = max(dp[j], dp[j - time] + score)
+
+print(dp[T])
